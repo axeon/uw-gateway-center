fd28885dd3ea355f0a0f83454386201862754a77
zhangjin <23231269@qq.com>
2025-09-15 17:58:56 +0800

    refactor(uw): 重构应用启动方式并更新版本
    - 使用 AppBootStrap 统一应用启动逻辑
    - 更新 uw-base版本至 2025.0901.0000
    - 移除自定义 BeanNameGenerator
    - 更新应用版本:
      - uw-auth-center: 6.1.11 -> 6.1.12
      - uw-code-center: 1.1.5 -> 1.1.6
      - uw-gateway: 1.1.7 -> 1.1.8
      - uw-gateway-center:1.1.5 -> 1.1.6
      - uw-lms-center: 2.1.5 -> 2.1.6
      - uw-mydb-center: 3.1.5 -> 3.1.6
      - uw-ops-center: 1.1.8 -> 1.1.9
      - uw-task-center: 5.1.5 -> 5.1.6  - uw-tinyurl-center: 更新 nacos 配置密码

--- src/main/java/uw/gateway/center/UwGatewayCenterApplication.java
+++ src/main/java/uw/gateway/center/UwGatewayCenterApplication.java
@@ -1,31 +1,15 @@
 package uw.gateway.center;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+import uw.common.app.AppBootStrap;
 
-/**
- * UwAuthCenterApplication
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UwGatewayCenterApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(UwGatewayCenterApplication.class).beanNameGenerator((beanDefinition, beanDefinitionRegistry) -> {
-            String beanClassName = beanDefinition.getBeanClassName();
-            if (beanClassName.startsWith("uw.gateway")) {
-                return beanClassName;
-            }
-
-            if (beanClassName.endsWith("LoadBalancerAutoConfiguration")) {
-                return beanClassName;
-            }
-
-            return new AnnotationBeanNameGenerator().generateBeanName(beanDefinition, beanDefinitionRegistry);
-        }).run(args);
-
+        AppBootStrap.run(UwGatewayCenterApplication.class, args);
     }
 
 }
